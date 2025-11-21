79e8c130cc62b525c5cd416080673b2fce5e7b97
Juan Fernando Rosero <juanfer46934@gmail.com>
2025-11-21 15:17:59 -0500

    feat: Add findByEmail method to UsuarioRepository and update createdFiles.lst

--- clinica-backend/src/main/java/com/clinica/project/infrastructure/repository/UsuarioRepository.java
+++ clinica-backend/src/main/java/com/clinica/project/infrastructure/repository/UsuarioRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByUsername(String username);
+    Optional<Usuario> findByEmail(String email);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
